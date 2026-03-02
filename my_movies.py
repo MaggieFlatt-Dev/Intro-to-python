a2aff18f9eaac08a12d08313b385607775837bc2
Maggie Flatt <maggieflatt19@gmail.com>
2026-03-02 15:47:26 -0600

    finished planets exercise and fixed spacing in my_movies.py & todos.py

--- my_movies.py
+++ my_movies.py
@@ -1,12 +1,14 @@
-# Initialize and Empty List 
+# Initialize and Empty List
 favorite_movies = []
 
-# Function to add a movie to List 
+
+# Function to add a movie to List
 def add_movie(movie):
     favorite_movies.append(movie)
     print(f"Movie '{movie}' added")
 
-# Function to remove movie from list 
+
+# Function to remove movie from list
 def remove_movie(movie):
     if movie in favorite_movies:
         favorite_movies.remove(movie)
@@ -14,16 +16,19 @@ def remove_movie(movie):
     else:
         print(f"Movie '{movie}' not found")
 
+
 # Function to display movies in list
 def display_movies():
     print("Favorite Movies:")
     for movie in favorite_movies:
         print(f"- '{movie}'")
 
-# Function to count movies in the list 
+
+# Function to count movies in the list
 def count_movies():
     total_movies = len(favorite_movies)
-    print(total_movies) 
+    print(total_movies)
+
 
 # Function to find a movie
 def find_movie(movie):
@@ -32,12 +37,14 @@ def find_movie(movie):
     else:
         print("Movie not found")
 
-# Clear the list 
+
+# Clear the list
 def clear_movies():
     favorite_movies.clear()
     print("List has been cleared")
 
-# Adding movies 
+
+# Adding movies
 add_movie("A League of Their Own")
 add_movie("Princess and the Frog")
 add_movie("Beauty and The Beast")
@@ -45,7 +52,7 @@ add_movie("I Love You, Man")
 add_movie("Forgetting Sarah Marshall")
 add_movie("Lord of The Rings: Fellowship of The Ring")
 
-#Space to make it easier to read
+# Space to make it easier to read
 print()
 
 # Display List
@@ -56,19 +63,19 @@ print()
 remove_movie("Forgetting Sarah Marshall")
 print()
 
-# Display List 
+# Display List
 display_movies()
 print()
 
-# Count movies in list 
+# Count movies in list
 count_movies()
 print()
 
-# Find movie in list 
+# Find movie in list
 find_movie("A League of Their Own")
 print()
 
-# Clear the List 
+# Clear the List
 clear_movies()
 print()
 display_movies()
--- planets.py
+++ planets.py
@@ -1,1 +1,50 @@
 planet_list = ["Mercury", "Mars"]
+planets = ["Neptune", "Uranus"]
+
+# Display Planets
+def display_planets():
+    print("Planets:")
+    for planet in planet_list:
+        print(f"- {planet}")
+
+# Use append() to add Jupiter and Saturn to the end of the list
+def add_planet(planet):
+    planet_list.append(planet)
+    print(f"{planet} added")
+
+
+# Display Planet List
+display_planets()
+print()
+
+# Add planets
+add_planet("Jupiter")
+add_planet("Saturn")
+print()
+display_planets()
+
+# Use the extend() method to add another list of the last two planets in our solar system to the end of the list.
+print()
+planet_list.extend(planets)
+display_planets()
+
+# Use insert() to add Earth, and Venus in the correct order.
+print()
+planet_list.insert(1, "Venus")
+planet_list.insert(2, "Earth")
+display_planets()
+
+# Use append() again to add Pluto to the end of the list.
+print()
+planet_list.append("Pluto")
+display_planets()
+
+# Now that all the planets are in the list, slice the list in order to get the rocky planets into a new list called rocky_planets. 
+print()
+rocky_planets = planet_list[0:4]
+print(f"Rocky Planets = {rocky_planets}")
+
+# Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the del operation to remove it from the end of planet_list.
+print()
+del planet_list[8]
+display_planets()
